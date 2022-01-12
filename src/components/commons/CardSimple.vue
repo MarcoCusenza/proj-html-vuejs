ba1e6d90a521877fcb95d669d4e42cc60c848f9b
Marco Cusenza <marco.cusenza.me@gmail.com>
2022-01-12 18:24:49 +0100

    Mixins + Section Company

---
+++ src/components/commons/CardSimple.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="card" :class="className">
+    <div class="title">
+      <i :class="obj.logo" class="icon"></i>
+      <h3>{{ obj.title }}</h3>
+    </div>
+    <div class="subtitle">{{ obj.subtitle }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CardCompany",
+  props: {
+    className: String,
+    obj: Object,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../../assets/style/partials/variables.scss";
+@import "../../assets/style/mixins/mixin.scss";
+
+@include cardCompany;
+</style>
--- src/components/sections/Company.vue
+++ src/components/sections/Company.vue
@@ -3,27 +3,32 @@
     <div class="container">
       <div class="box">
         <SectionTitle :obj="titleData" />
-        <div class="text">Tempore sapiente nemo temporibus aperiam error at. Voluptas, libero. A, debitis reprehenderit.</div>
+        <div class="text">
+          Tempore sapiente nemo temporibus aperiam error at. Voluptas, libero.
+          A, debitis reprehenderit.
+        </div>
       </div>
       <div class="box">
-        <CardCompany />
-        <CardCompany />
-        <CardCompany />
-        <CardCompany />
+        <CardSimple
+          v-for="(card, i) in cardsData"
+          :key="i"
+          :className="'card-company'"
+          :obj="card"
+        />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import CardCompany from "../commons/CardCompany.vue";
+import CardSimple from "../commons/CardSimple.vue";
 import SectionTitle from "../commons/SectionTitle.vue";
 
 export default {
   name: "Company",
   components: {
     SectionTitle,
-    CardCompany,
+    CardSimple,
   },
   data() {
     return {
@@ -32,6 +37,28 @@ export default {
         title: "The Company",
         subtitle:
           "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam, fuga atque dignissimos hic non illum ducimus totam praesentium voluptate magni molestiae impedit ex labore maiores amet alias quasi nobis sapiente?",
+      },
+      cardsData: {
+        card1: {
+          logo: "fas fa-award",
+          title: "Tradition",
+          subtitle: "Lorem ipsum dolor sit amet consectetur adipiscing elit.",
+        },
+        card2: {
+          logo: "fas fa-lock",
+          title: "Security",
+          subtitle: "Lorem ipsum dolor sit amet consectetur adipiscing elit.",
+        },
+        card3: {
+          logo: "fas fa-edit",
+          title: "Certificate",
+          subtitle: "Lorem ipsum dolor sit amet consectetur adipiscing elit.",
+        },
+        card4: {
+          logo: "fas fa-graduation-cap",
+          title: "Expertise",
+          subtitle: "Lorem ipsum dolor sit amet consectetur adipiscing elit.",
+        },
       },
     };
   },
@@ -43,17 +70,26 @@ export default {
 
 .company {
   padding: 100px;
+  .container {
+    display: grid;
+    grid-template-columns: 50% 50%;
 
-  .box {
-    width: 50%;
+    .box {
+      .text {
+        font-size: 0.875rem;
+        color: $text-light-low-contrast;
+        margin-top: 20px;
+      }
 
-    .text{
-    font-size: .875rem;
-    color: $text-light-low-contrast;
-    margin-top: 20px;
-  }
+      &:first-of-type{
+        width: 80%;
+      }
 
-    
+      &:last-of-type {
+        display: grid;
+        grid-template-columns: 50% 50%;
+      }
+    }
   }
 }
 </style>
